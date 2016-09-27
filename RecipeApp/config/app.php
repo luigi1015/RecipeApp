cb83d8b4c738f822eb7cff4d9cfcb3965b3270b7
Jeff <github@314ghz.com>
2016-09-26 19:54:50 -0500

    Did some basic configuration.

--- RecipeApp/config/app.php
+++ RecipeApp/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Recipe',
 
     /*
     |--------------------------------------------------------------------------
@@ -120,7 +120,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    //'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'daily'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
